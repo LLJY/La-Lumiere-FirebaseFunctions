5594dfdb9289733e4783b490d00f6eb83c29d615
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2020-05-21 01:43:36 +0800

    Add getUserItems function + capability to run on localhost
    I spent too much time on this

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,141 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+var firebaseConfig = {
+    apiKey: "AIzaSyBi0vKkdYtAclkL9-ZYYn-mBZmCu6GOGjU",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appId: "app-id",
+    measurementId: "G-measurement-id",
+};
+admin.initializeApp();
+const db = admin.firestore();
+class Item {
+    //this is the items class we will pass to the app.
+    public Title: string;
+    public Seller: Seller;
+    public Likes: number;
+    public ListedTime: Date;
+    public Rating: number;
+    public Description: string;
+    public TranscationInformation: string;
+    public ProcurementInformation: string;
+    public PaymentType: string;
+    public Category: string;
+    public Stock: number;
+    public Images: Array<string>;
+    public Performance: number;
+    public isAdvert: boolean;
+    //mapped to an enum
+    public StockStatus: number;
+    constructor(Title: string, Seller: Seller, Likes: number, ListedTime: Date, Rating: number, Description: string, TransactionInformation: string, ProcurementInformation: string, PaymentType: string, Category: string, Stock: number, Image1: string, Image2: string, Image3: string, Image4: string, AdvertisementPoints: number, isDiscounted: boolean, isRestocked: boolean) {
+        this.Title = Title;
+        this.Seller = Seller;
+        this.Likes = Likes;
+        this.ListedTime = ListedTime;
+        this.Rating = Rating;
+        this.Description = Description;
+        this.TranscationInformation = TransactionInformation;
+        this.ProcurementInformation = ProcurementInformation;
+        this.PaymentType = PaymentType;
+        this.Category = Category;
+        this.Stock = Stock;
+        //convert stock status into the enum for the application
+        //it is intended for isDiscounted to take priority over the rest as it contributes to the most score in the algorithm
+        if (isDiscounted) {
+            this.StockStatus = 4
+        } else if (Stock <= 10) {
+            //STATUS_RUNNING_OUT
+            this.StockStatus = 3;
+        } else if (isRestocked) {
+            this.StockStatus = 2
+        } else {
+            this.StockStatus = 1
+        }
+        const images = new Array<string>();
+        //if the image urls are null or empty, do not add them to the list.
+        if (Image1 as string) {
+            images.push(Image1);
+        }
+        if (Image2 as string) {
+            images.push(Image2);
+        }
+        if (Image3 as string) {
+            images.push(Image3);
+        }
+        if (Image4 as string) {
+            images.push(Image4);
+        }
+        this.Images = images;
+        //performance on the list is calculated by number of likes over time multiplied by advertisement points
+        this.Performance = (Likes / ((new Date).valueOf() - ListedTime.valueOf()) * Math.max(AdvertisementPoints, 1));
+    }
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+}
+class Seller {
+    Name: string;
+    UID: string;
+    pictureURL: string;
+
+    constructor(name: string, uid: string, pictureurl: string) {
+        this.Name = name;
+        this.UID = uid;
+        this.pictureURL = pictureurl;
+    }
+
+}
+
+export const getUserItems = functions.https.onRequest((data, response) => {
+    var arrayItem = new Array<any>();
+    var itemSeller: Seller;
+    //add a dummy limit for testing
+    //let limit = 12;
+    //we do not have a system for user preference, so for now just list all items.
+    const itemsRef = db.collection('users');
+    itemsRef.get().then(sellerSnapshot => {
+        //promises with the right datatype (not necessary but good practice)
+        const promises = new Array<Promise<FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>>>();
+        sellerSnapshot.forEach(sellerDoc => {
+            const sellerData = sellerDoc.data();
+            //check for non null / empty strings
+            if (sellerData.Name as string && sellerData.UID as string) {
+                //this is all the seller information we need
+                itemSeller = new Seller(sellerData.Name, sellerData.UID, ""); //placeholder profile picture
+                var refItem = sellerDoc.ref.collection('Items')
+                //push all the promises to a list so we can run all our queries in parallel
+                promises.push(refItem.get());
+            }
+        });
+        //return the promise so we can run a .then and "await" for them all
+        return Promise.all(promises)
+    })
+        //going through the promises
+        .then(itemPromise => {
+            //for every snapshot
+            itemPromise.forEach(itemSnapshot => {
+                //for every item
+                itemSnapshot.forEach(itemDoc => {
+                    //get the data
+                    var itemData = itemDoc.data();
+                    //if title is not null, the rest of the fields are unlikely to be.
+                    if (itemData.Title as string) {
+                        //the rest of the logic to convert from database to model is in the constructor
+                        arrayItem.push(new Item(itemData.Title, itemSeller, itemData.Likes, itemData.ListedTime, itemData.Rating, itemData.Description, itemData.TransactionInformation, itemData.ProcurementInformation, itemData.PaymentType, itemData.Category, itemData.Stock, itemData.Image1, itemData.Image2, itemData.Image3, itemData.Image4, itemData.AdvertisementPoints, itemData.isDiscounted, itemData.isRestocked));
+                    }
+                });
+            });
+            arrayItem.sort(x => x.Performance);
+            response.send(arrayItem);
+        }).catch(error => {
+            console.error(error);
+            response.send(error);
+        });
+});
+export const helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
