f63535ef5d32107c2e7254e4008bcec50fe47887
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2020-05-21 02:36:16 +0800

    add firebase and run file

---
+++ run.sh
@@ -0,0 +1,3 @@
+export GOOGLE_APPLICATION_CREDENTIALS="/home/lucas/Projects/MBAP/La-Lumiere/Firebase-Functions/firebasecfg.json"   
+npm --prefix /home/lucas/Projects/MBAP/La-Lumiere/Firebase-Functions/functions/ run build
+firebase serve --only functions
